8017759afb14369c975e2a97d9584c5fca7c40f6
Nickolay Smirnov <Nickolay.Smirnov@hotmail.com>
2022-06-07 22:54:22 +0100

    Pull Stimulus and Read Time from sheet

--- manipulator.py
+++ manipulator.py
@@ -7,23 +7,29 @@ from os import makedirs
 def main():
     parser = ArgumentParser()
     parser.add_argument('file')
-    parser.add_argument('stimulus')
     parser = parser.parse_args()
     in_name = parser.file
-    stimulus = parser.stimulus
     dir_name = in_name[:in_name.index('.')]
     makedirs(dir_name, exist_ok=True)
     for sample_id, sheet in read_excel(in_name, None).items():
+        if 'alpha' in sample_id:
+            stimulus = 'alpha'
+        elif 'omega' in sample_id:
+            stimulus = 'omega'
+        else:
+            stimulus = 'unknown'
+            print(f'Unknown stimulus for sheet {sample_id}')
         with open(f'{dir_name}/{sample_id}.csv', 'w') as f:
             f.write(
-                'date,plate,well,serum_percent,stimulus,SAMPLE_ID,absorbance_620,stimulus\n')
+                'date,plate,well,serum_percent,stimulus,SAMPLE_ID,absorbance_620,stimulus,Read time\n')
+            read_time = sheet.iloc[6, 0][6:]
             for i, row in sheet.iterrows():
                 if not (isnan(row[-1]) or row[0] != row[0]):
                     row_letter = None
                     for i, cell in enumerate(row):
                         if row_letter:
                             f.write(
-                                f',,{row_letter}{i},,,{sample_id},{cell},{stimulus}\n')
+                                f',,{row_letter}{i},,,{sample_id},{cell},{stimulus},{read_time}\n')
                         else:
                             row_letter = cell
 
